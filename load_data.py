70572bfdf99bea6d84a6b8ad3bffdd4a4b0d0476
Iman Irajdoost <iman@seven-shapes.com>
2024-01-23 12:13:41 +0100

    [FEAT] Added analyse data (description, plot) + fixed some object data types

--- load_data.py
+++ load_data.py
@@ -15,6 +15,50 @@ def load_data(file_path):
 
             # If the JSON data is nested, you might need to normalize it:
             df = pd.json_normalize(data)
+
+            # correct the date object type
+
+            # Convert 'elevator' column to boolean
+            df['elevator'] = df['elevator'].astype(bool)
+
+            # Convert 'price' column to numeric and explicitly remove NaN values
+            df['price'] = pd.to_numeric(df['price'], errors='coerce')
+
+            df['bedroom'] = pd.to_numeric(df['bedroom'], errors='coerce')
+
+            df['description'] = df['description'].astype(str)
+
+            df['createdAt'] = pd.to_datetime(df['createdAt'], errors='coerce')
+
+            df['expirte'] = df['expired'].astype(bool)
+
+            df['floor'] = pd.to_numeric(df['floor'], errors='coerce')
+
+            df['furnished'] = df['furnished'].astype(bool)
+
+            df['pricePerMeter'] = pd.to_numeric(df['pricePerMeter'], errors='coerce')
+
+            df['pricePerMeter'] = pd.to_numeric(df['pricePerMeter'], errors='coerce')
+
+            df['room'] = pd.to_numeric(df['room'], errors='coerce')
+
+            df['surface'] = pd.to_numeric(df['surface'], errors='coerce')
+
+            df['title'] = df['title'].astype(str)
+
+            df['city.department.code'] = pd.to_numeric(df['city.department.code'], errors='coerce')
+
+            df['city.department.name'] = df['city.department.name'].astype(str)
+
+            df['city.location.lat'] = pd.to_numeric(df['city.location.lat'], errors='coerce')
+
+            df['city.location.lon'] = pd.to_numeric(df['city.location.lon'], errors='coerce')
+
+            df['city.name'] = df['city.name'].astype(str)
+
+            df['city.zipcode'] = pd.to_numeric(df['city.zipcode'], errors='coerce')
+
+
             return df
 
     except json.JSONDecodeError as err:
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from load_data import load_data
-from flatten_data import flatten_data
 from preprocess_data import preprocess_data
+from analyse_data import analyse
 
 file_path = 'data/cities/data-paris.json' # Update this to the path of your JSON file
 
 # Load the data
 df = load_data(file_path)
 
+# Show the analysis of the data
+analyse(df)
+
 # Preprocess the data
-df_preprocessed = preprocess_data(df)
+# df_preprocessed = preprocess_data(df)
 
 # df_preprocessed is now ready for further analysis or modeling
