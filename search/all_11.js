a6e3e6faf7abeef63b9cce03ab4e66740bed339f
imanirajdoost <imanirajdoost@users.noreply.github.com>
2024-05-02 09:59:08 +0000

    deploy: 256b3bc1dd02ec4a1d91407b36f3872c4e23b5ff

--- search/all_11.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['voting_5fregressor_0',['voting_regressor',['../namespacevoting__regressor.html',1,'']]],
-  ['voting_5fregressor_5ftrain_5fmodel_1',['voting_regressor_train_model',['../namespacevoting__regressor.html#a0226bd8b87fc7b7a27acebfa24038286',1,'voting_regressor']]]
-];
